ef130bf032187528ec404f513d09a83d21952130
everlane <weilinchen@college.harvard.edu>
2012-12-08 06:11:57 -0500

    end of day two, setting up ecoli applet

--- applet5ecoli/ecoli/ecoliApplet.java
+++ applet5ecoli/ecoli/ecoliApplet.java
@@ -12,70 +12,79 @@ public class ecoliApplet extends Applet implements ActionListener {
 	
 	Dimension size;
 	
-	// keeps track of current page
-	public int currPage;
+	// input panel for applet layout
+	Panel inputPanel;
 	
-	// layout for applet
-	// main Panel
-	CardLayout mainLayout;
-	Panel mainPanel;
-	Panel page0, page1;
-	
-	// bottom map
-	Panel roadMap;
-	
-	// buttons for bottom map
-	Button button0, button1;
-	
-	
+	// textfiels for input panel
 	TextField dimInput;
+	TextField cNumInput;
+	TextField rewardInput;
+	TextField suckerInput;
+	TextField temptationInput;
+	TextField punishmentInput;
 	
+	// button for submit inputs
+	Button submitButton;
+	
+	// constants for game
+	boolean gameOn = false;
+	
+	int dimNum = 0;
+	int cNum = 0;
+	int rewardNum = 0;
+	int suckerNum = 0;
+	int temptationNum = 0;
+	int punishmentNum = 0;
+	 
 	public void init()
 	{
 		// set size of applet
 		this.width = getSize().width;
 		this.height = getSize().height;
 		
+		// sets size of applet
 		size = new Dimension(500,300);
 		
 		setSize(size);
 		
-		this.currPage = 0;
-		
-		// make holder for main slides
-		mainPanel = new Panel();
-		mainLayout = new CardLayout();
-		mainPanel.setLayout(mainLayout);
-		
-		// make slides
-		page0 = new Panel();
-		page1 = new Panel();
-		
-		dimInput = new TextField(20);
+		// create textfields and submit button
+		dimInput = new TextField(5);
 		dimInput.setText("0");
-		page1.add(dimInput);
 		
-		// add slides to main holder
-		mainPanel.add(page0, "page 0");
-		mainPanel.add(page1, "page 1");
+		cNumInput = new TextField(5);
+		cNumInput.setText("0");
 		
-		// make road maps
-		roadMap = new Panel();
+		rewardInput = new TextField(5);
+		rewardInput.setText("0");
 		
-		// add buttons to road map
-		button0 = new Button("1");
-		button0.addActionListener(this);
-		button1 = new Button("2");
-		button1.addActionListener(this);
+		suckerInput = new TextField(5);
+		suckerInput.setText("0");
 		
-		roadMap.add(button0);
-		roadMap.add(button1);
+		temptationInput = new TextField(5);
+		temptationInput.setText("0");
+		
+		punishmentInput = new TextField(5);
+		punishmentInput.setText("0");
+		
+		submitButton = new Button("Go");
+		submitButton.addActionListener(this);
+		
+		// creates the input panel (Flow style) and add fields
+		inputPanel = new Panel();
+		
+		inputPanel.add(dimInput);
+		inputPanel.add(cNumInput);
+		inputPanel.add(rewardInput);
+		inputPanel.add(suckerInput);
+		inputPanel.add(temptationInput);
+		inputPanel.add(punishmentInput);
+		inputPanel.add(submitButton);
 		
 		// overall layout is Border style, add main and roadmap
 		this.setLayout(new BorderLayout());
-		this.add(roadMap, BorderLayout.SOUTH);
-		this.add(mainPanel, BorderLayout.CENTER);
+		this.add(inputPanel, BorderLayout.SOUTH);
 		
+		// set background
 		setBackground(Color.white);
 		
 	}
@@ -84,33 +93,44 @@ public class ecoliApplet extends Applet implements ActionListener {
 	public void actionPerformed(ActionEvent event)
 	{
 		// actions for roadmap buttons
-		if (event.getSource() == button0)
+		if (event.getSource() == submitButton)
 		{
-			currPage = 0;
-			mainLayout.show(mainPanel, "page 0");
+			String dimS = dimInput.getText();
+			String cNumS = cNumInput.getText();
+			String rewardS = rewardInput.getText();
+			String suckerS = suckerInput.getText();
+			String temptationS = temptationInput.getText();
+			String punishmentS = punishmentInput.getText();
+			
+			dimNum = Integer.parseInt(dimS);
+			cNum = Integer.parseInt(cNumS);
+			rewardNum = Integer.parseInt(rewardS);
+			suckerNum = Integer.parseInt(suckerS);
+			temptationNum = Integer.parseInt(temptationS);
+			punishmentNum = Integer.parseInt(punishmentS);
+			
+			gameOn = true;
 			repaint();
 		}
-		
-		if (event.getSource() == button1)
-		{
-			currPage = 1;
-			mainLayout.show(mainPanel, "page 1");
-		}
 	}
-	
+
 	// paint function
 	public void paint (Graphics g)
 	{
 		Graphics2D g2 = (Graphics2D) g;
-		if (currPage == 0)
+		if (!gameOn)
 		{
 			Font f = new Font("Monospaced", Font.BOLD, 20);
 			g2.setFont (f);
 			g2.setColor(Color.black);
-			//g.drawRect(10, 10, 10, 10);
-			g2.drawString("Evolutionary Games on Graphs with Examples", 10, 10);
-			
+			g2.drawString("Evolutionary Games on Graphs with Examples", 20, 20);
+	
 		}
 	}
+	// run an e.coli game
+	/*public void gameEcoli ()
+	{
+		Grid game = new Grid()
+	}*/
 
 }
